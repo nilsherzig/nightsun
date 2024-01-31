44fd121d1f4b8fcfd92f70b2fd12002537fdfc3c
Nils Herzig <git@nilsherzig.com>
2024-01-31 15:14:21 +0100

    hotreload

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"sync"
 
 	"github.com/ktr0731/go-fuzzyfinder"
 	"github.com/pkg/errors"
@@ -14,35 +15,37 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	var mut sync.RWMutex
 	items := []Item{}
-	for _, module := range config.Modules {
-		if len(module.Producer) > 0 {
-			cmd := exec.Command("bash", "-c", "set -euo pipefail;"+module.Producer)
-			out, err := cmd.Output()
-			if err != nil {
-				log.Print(errors.Wrapf(err, "%v: command failed", module.Producer))
-				continue
-			}
+	go func(itemptr *[]Item) {
+		for _, module := range config.Modules {
+			if len(module.Producer) > 0 {
+				cmd := exec.Command("bash", "-c", "set -euo pipefail;"+module.Producer)
+				out, err := cmd.Output()
+				if err != nil {
+					log.Print(errors.Wrapf(err, "%v: command failed", module.Producer))
+					continue
+				}
 
-			trimmed := strings.TrimSpace(string(out))
-			if len(trimmed) <= 0 {
-				continue
-			}
+				trimmed := strings.TrimSpace(string(out))
+				if len(trimmed) <= 0 {
+					continue
+				}
 
-			lines := strings.Split(trimmed, "\n")
-			log.Print(len(lines))
-			for _, line := range lines {
-				items = append(items, Item{
-					Module: module,
-					Line:   strings.ReplaceAll(line, "\t", "    "),
-				})
+				lines := strings.Split(trimmed, "\n")
+				log.Print(len(lines))
+				for _, line := range lines {
+					items = append(items, Item{
+						Module: module,
+						Line:   strings.ReplaceAll(line, "\t", "    "),
+					})
+				}
 			}
 		}
-	}
+	}(&items)
 
 	idx, err := fuzzyfinder.Find(
-		items,
+		&items,
 		func(i int) string {
 			return items[i].Module.Prefix + items[i].Line
 		},
@@ -52,6 +55,7 @@ func main() {
 			}
 			return items[i].Module.Show()
 		}),
+		fuzzyfinder.WithHotReloadLock(mut.RLocker()),
 	)
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "selection failed"))
