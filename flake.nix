31ce7862b8447b3565e11b090fa085eb43a23acf
Leon Schumacher <leonsch@protonmail.com>
2024-01-31 12:50:05 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in rec {
+        defaultPackage = pkgs.buildGoModule {
+          pname = "unified-search";
+          version = "0";
+          src = ./.;
+          vendorHash = "sha256-U/AuOIC1eYVi+lwjD5jS8lDInqnwOwdlq0X+9bC3R7c=";
+        };
+
+        devShell = pkgs.mkShell {
+          inputsFrom = [ defaultPackage ];
+          packages = with pkgs; [
+            gopls
+          ];
+        };
+      });
+}
